2f457cabc041990c3f141c5f41d91667ba518b1c
Jay Convertino <johnathan.convertino.1@us.af.mil>
2023-02-13 10:39:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# AXIS 1553 ENCODER
+## AXIS TO MIL-STD-1553
+---
+
+   author: Jay Convertino   
+   
+   date: 2021.05.17  
+   
+   details: Incoming AXI streaming data is used to create a MIL-STD-1553 compliant signal.   
+   
+   license: MIT   
+   
+---
+
+![rtl_img](./rtl.png)
+
+### Dependencies
+#### Build
+
+  - AFRL:utility:helper:1.0.0
+  
+#### Simulation
+
+  - AFRL:simulation:axis_stimulator
+
+### IP USAGE
+#### INSTRUCTIONS
+
+TDATA input should contain the 16 bit data payload. TUSER is a 8 bit command register   
+that takes a discription what type of data it is (command or data) and other options.   
+described below.   
+
+* TUSER = {TYY,NA,D,I,P} (7 downto 0)
+  * TYY = TYPE OF DATA
+    * 000 N/A
+    * 001 REG (NOT IMPLIMENTED)
+    * 010 DATA
+    * 100 CMD/STATUS
+  * NA = RESERVED FOR FUTURE USE.
+  * D = DELAY ENABLED
+    * 1 = 4 us delay enabled.
+    * 0 = no delay between transmissions.
+  * I = INVERT DATA
+    * 1 = Invert data.
+    * 0 = Normal data.
+  * P = PARITY
+    * 1 = ODD
+    * 0 = EVEN
+
+#### PARAMETERS
+
+* clock_speed : DEFAULT = 2000000 : clock speed of aclk to the core in hz.
+* sample_rate : DEFAULT = 2000000 : sample rate of generated signal in hz (minimum 2 MHz).
+
+### COMPONENTS
+#### SRC
+
+* axis_1553_encoder.v
+  
+#### TB
+
+* tb_1553_enc.vhd
+  
+### fusesoc
+
+* fusesoc_info.core created.
+* Simulation uses icarus to run data through the core.
---
+++ tb/tb_1553_enc.v
@@ -0,0 +1,112 @@
+//******************************************************************************
+/// @file    tb_1553_enc.v
+/// @author  JAY CONVERTINO
+/// @date    2021.05.25
+/// @brief   SIMPLE TEST BENCH FOR 1553... THIS NEEDS MORE... STUFF. 8^)
+///
+/// @LICENSE MIT
+///  Copyright 2021 Jay Convertino
+///
+///  Permission is hereby granted, free of charge, to any person obtaining a copy
+///  of this software and associated documentation files (the "Software"), to 
+///  deal in the Software without restriction, including without limitation the
+///  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+///  sell copies of the Software, and to permit persons to whom the Software is 
+///  furnished to do so, subject to the following conditions:
+///
+///  The above copyright notice and this permission notice shall be included in 
+///  all copies or substantial portions of the Software.
+///
+///  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+///  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+///  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+///  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+///  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+///  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+///  IN THE SOFTWARE.
+//******************************************************************************
+
+`timescale 1 ns/10 ps
+
+module tb_1553;
+  
+  reg         tb_data_clk = 0;
+  reg         tb_rst = 0;
+  wire [1:0]  tb_dout;
+  wire        tb_en_dout;
+  wire [15:0] tb_tdata;
+  wire        tb_tvalid;
+  wire        tb_tready;
+  
+  //1ns
+  localparam CLK_PERIOD = 50;
+  localparam RST_PERIOD = 100;
+  
+  //produce data
+  slave_axis_stimulus #(
+    .BUS_WIDTH(2),
+    .USER_WIDTH(8),
+    .DEST_WIDTH(1),
+    .FILE("in.bin")
+  ) slave_axis_stim (
+    // output to slave
+    .m_axis_aclk(tb_data_clk),
+    .m_axis_arstn(~tb_rst),
+    .m_axis_tvalid(tb_tvalid),
+    .m_axis_tready(tb_tready),
+    .m_axis_tdata(tb_tdata),
+    .m_axis_tkeep(),
+    .m_axis_tlast(),
+    .m_axis_tuser(),
+    .m_axis_tdest()
+  );
+  
+  //device under test
+  axis_1553_encoder #(
+    .CLOCK_SPEED(20000000)
+  ) dut (
+    .aclk(tb_data_clk),
+    .arstn(~tb_rst),
+    //slave input
+    .s_axis_tdata(tb_tdata),
+    .s_axis_tvalid(tb_tvalid),
+    .s_axis_tuser(8'h81),
+    .s_axis_tready(tb_tready),
+    //diff output
+    .diff(tb_dout),
+    .en_diff(tb_en_dout)
+  );
+    
+  //reset
+  initial
+  begin
+    tb_rst <= 1'b1;
+    
+    #RST_PERIOD;
+    
+    tb_rst <= 1'b0;
+  end
+  
+  //copy pasta, fst generation
+  initial
+  begin
+    $dumpfile("tb_1553_enc.fst");
+    $dumpvars(0,tb_1553);
+  end
+  
+  //clock
+  always
+  begin
+    tb_data_clk <= ~tb_data_clk;
+    
+    #(CLK_PERIOD/2);
+  end
+  
+  //copy pasta, no way to set runtime... this works in vivado as well.
+  initial begin
+    #10_000_000; // Wait a long time in simulation units (adjust as needed).
+    $display("END SIMULATION");
+    $finish;
+  end
+endmodule
+
